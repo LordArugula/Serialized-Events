f65465c66807ab8a6312181b11b1dcd925762b96
Victor Pan <victorpan2017@gmail.com>
2022-06-30 12:33:40 -0700

    Update SerializedEventDrawer header
    SerializedEventDrawer draws headers with the return type and parameter types

--- Editor/SerializedEventDrawer.cs
+++ Editor/SerializedEventDrawer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reflection;
+using System.Text;
 
 using UnityEditor;
 
@@ -12,6 +14,7 @@ namespace Arugula.SerializedEvents.Editor
     public class SerializedEventDrawer : PropertyDrawer
     {
         private ReorderableList reorderableList;
+        private StringBuilder builder;
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
@@ -22,39 +25,6 @@ namespace Arugula.SerializedEvents.Editor
             return reorderableList.GetHeight();
         }
 
-        private void InitializeList(SerializedProperty property, GUIContent label)
-        {
-            SerializedProperty callbackProperty = property.FindPropertyRelative("callback");
-            SerializedProperty delegateListProperty = callbackProperty.FindPropertyRelative("delegates");
-
-            reorderableList = new ReorderableList(property.serializedObject,
-                delegateListProperty,
-                true,
-                true,
-                true,
-                true)
-            {
-                drawHeaderCallback = (rect) =>
-                {
-                    EditorGUI.LabelField(rect, label);
-                },
-                drawElementCallback = (rect, index, _, _) =>
-                {
-                    SerializedProperty property = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
-                    EditorGUI.PropertyField(rect, property, true);
-                },
-                elementHeight = EditorGUIUtility.singleLineHeight + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing,
-                onAddCallback = (list) =>
-                {
-                    int count = list.serializedProperty.arraySize;
-                    list.serializedProperty.InsertArrayElementAtIndex(count);
-                    SerializedProperty eventProperty = list.serializedProperty.GetArrayElementAtIndex(count);
-                    eventProperty.FindPropertyRelative("target").objectReferenceValue = null;
-                    eventProperty.FindPropertyRelative("methodName").stringValue = null;
-                },
-            };
-        }
-
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (reorderableList == null)
@@ -63,6 +33,104 @@ namespace Arugula.SerializedEvents.Editor
             }
 
             reorderableList.DoList(position);
+        }
+
+        private void InitializeList(SerializedProperty property, GUIContent label)
+        {
+            SerializedProperty callbackProperty = property.FindPropertyRelative("callback");
+            SerializedProperty delegateListProperty = callbackProperty.FindPropertyRelative("delegates");
+
+            reorderableList = new ReorderableList(
+                property.serializedObject,
+                delegateListProperty,
+                true,
+                true,
+                true,
+                true)
+            {
+                drawHeaderCallback = (rect) => DrawHeader(rect, label),
+                drawElementCallback = DrawElement,
+                elementHeight = EditorGUIUtility.singleLineHeight + EditorGUIUtility.singleLineHeight + EditorGUIUtility.standardVerticalSpacing,
+                onAddCallback = AddElement,
+            };
+        }
+
+        private void AddElement(ReorderableList list)
+        {
+            int count = list.serializedProperty.arraySize;
+            list.serializedProperty.InsertArrayElementAtIndex(count);
+            SerializedProperty eventProperty = list.serializedProperty.GetArrayElementAtIndex(count);
+            eventProperty.FindPropertyRelative("target").objectReferenceValue = null;
+            eventProperty.FindPropertyRelative("methodName").stringValue = null;
+        }
+
+        private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            SerializedProperty property = reorderableList.serializedProperty.GetArrayElementAtIndex(index);
+            EditorGUI.PropertyField(rect, property, true);
+        }
+
+        private void DrawHeader(Rect rect, GUIContent label)
+        {
+            Type fieldType = fieldInfo.FieldType;
+            MethodInfo invokeMethod = fieldType.GetMethod("Invoke", BindingFlags.Instance | BindingFlags.Public);
+            if (invokeMethod == null)
+            {
+                EditorGUI.LabelField(rect, label.text);
+                return;
+            }
+
+            string formattedMethodLabel = GetFormattedLabel(label.text, invokeMethod);
+            EditorGUI.LabelField(rect, new GUIContent(formattedMethodLabel));
+        }
+
+        private string GetFormattedLabel(string propertyDisplayName, MethodInfo invokeMethod)
+        {
+            if (builder == null)
+            {
+                builder = new StringBuilder();
+            }
+            else
+            {
+                builder.Clear();
+            }
+
+            Type returnType = invokeMethod.ReturnType;
+            if (returnType != typeof(void))
+            {
+                builder.Append(returnType.Name)
+                    .Append(' ');
+            }
+
+            builder.Append(propertyDisplayName);
+
+            ParameterInfo[] parameterInfos = invokeMethod.GetParameters();
+            builder.Append(' ')
+                .Append('(');
+
+            if (parameterInfos.Length > 0)
+            {
+                AppendParameters(builder, parameterInfos);
+            }
+
+            builder.Append(')');
+
+            string formattedMethodLabel = builder.ToString();
+            return formattedMethodLabel;
+
+            void AppendParameters(StringBuilder _builder, ParameterInfo[] _parameterInfos)
+            {
+                for (int i = 0; i < _parameterInfos.Length; i++)
+                {
+                    ParameterInfo parameter = _parameterInfos[i];
+                    _builder.Append(parameter.ParameterType.Name);
+                    if (i + 1 < _parameterInfos.Length)
+                    {
+                        _builder.Append(',')
+                            .Append(' ');
+                    }
+                }
+            }
         }
     }
 }
